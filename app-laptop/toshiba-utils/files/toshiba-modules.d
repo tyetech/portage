4ff90b4c7676c22e6877af6d2a9537a0ccc47a0c
Peter Johanson <latexer@gentoo.org>
2004-03-08 05:52:22 +0000

    Moving to app-laptop

---
+++ app-laptop/toshiba-utils/files/toshiba-modules.d
@@ -0,0 +1,2 @@
+# $Header: /usr/local/ssd/gentoo-x86/output/app-laptop/cvs-repo/gentoo-x86/app-laptop/toshiba-utils/files/Attic/toshiba-modules.d,v 1.1 2004/03/08 05:52:22 latexer Exp $
+alias char-major-10-181 toshiba
