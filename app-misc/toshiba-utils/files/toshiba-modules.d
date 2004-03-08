6bb30d598fb373494bce15021b868b766c60092f
Peter Johanson <latexer@gentoo.org>
2004-03-08 05:53:19 +0000

    Moved to app-laptop

--- app-misc/toshiba-utils/files/toshiba-modules.d
+++
@@ -1,2 +0,0 @@
-# $Header: /usr/local/ssd/gentoo-x86/output/app-misc/cvs-repo/gentoo-x86/app-misc/toshiba-utils/files/Attic/toshiba-modules.d,v 1.2 2002/05/04 01:22:37 woodchip Exp $
-alias char-major-10-181 toshiba
