09e4bdbd24e9fa92d5b2db1bad0f7c008758d510
Mike Frysinger <vapier@gentoo.org>
2004-05-14 02:25:03 +0000

    Have the daemon create a .pid file so that the init.d script can kill it and   make the xinetd entry refer to the service auth instead of identd #50775 by   Remco Barendse

---
+++ net-misc/midentd/files/midentd.conf.d
@@ -0,0 +1,6 @@
+# Copyright 1999-2004 Gentoo Technologies, Inc.
+# Distributed under the terms of the GNU General Public License v2
+# $Header: /usr/local/ssd/gentoo-x86/output/net-misc/cvs-repo/gentoo-x86/net-misc/midentd/files/midentd.conf.d,v 1.1 2004/05/14 02:25:03 vapier Exp $
+
+# Options to pass to midentd
+MIDENTD_OPTS="-u nobody"
