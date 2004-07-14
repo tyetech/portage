b7cce10530f079497b9ac3bc232c5c3056e2f397
Aron Griffis <agriffis@gentoo.org>
2004-07-14 23:44:02 +0000

    Gentoo Technologies -> Gentoo Foundation

--- net-ftp/vsftpd/files/vsftpd.conf.d
+++ net-ftp/vsftpd/files/vsftpd.conf.d
@@ -1,6 +1,6 @@
-# Copyright 2003 Gentoo Technologies, Inc.
+# Copyright 2003-2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License, v2
-# $Header: /usr/local/ssd/gentoo-x86/output/net-ftp/cvs-repo/gentoo-x86/net-ftp/vsftpd/files/Attic/vsftpd.conf.d,v 1.1 2003/09/03 01:41:05 rajiv Exp $
+# $Header: /usr/local/ssd/gentoo-x86/output/net-ftp/cvs-repo/gentoo-x86/net-ftp/vsftpd/files/Attic/vsftpd.conf.d,v 1.2 2004/07/14 23:44:02 agriffis Exp $
 
 # Config file for /etc/init.d/vsftpd
 
