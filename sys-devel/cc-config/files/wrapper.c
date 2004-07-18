e8e9af0bc3bfda4a6538222a8fdeeaa7bb47835d
Daniel Black <dragonheart@gentoo.org>
2004-07-18 04:43:22 +0000

    Manifest fix - files/wrapper.c size mismatch

--- sys-devel/cc-config/files/wrapper.c
+++ sys-devel/cc-config/files/wrapper.c
@@ -2,7 +2,7 @@
  * Copyright 1999-2003 Gentoo Technologies, Inc.
  * Distributed under the terms of the GNU General Public License v2
  * Author: Martin Schlemmer <azarah@gentoo.org>
- * $Header: /usr/local/ssd/gentoo-x86/output/sys-devel/cvs-repo/gentoo-x86/sys-devel/cc-config/files/Attic/wrapper.c,v 1.2 2003/01/13 23:20:22 azarah Exp $
+ * $Header: /usr/local/ssd/gentoo-x86/output/sys-devel/cvs-repo/gentoo-x86/sys-devel/cc-config/files/Attic/wrapper.c,v 1.3 2004/07/18 04:43:22 dragonheart Exp $
  */
 
 #define _REENTRANT
