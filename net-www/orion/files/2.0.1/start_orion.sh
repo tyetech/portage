e6c2048f56d8e7de27dad3c2b7a8cddccd27d493
Daniel Black <dragonheart@gentoo.org>
2004-07-18 04:26:56 +0000

    Manifest fix - files/2.0/stop_orion.sh size mismatch, files/2.0/orion.conf size mismatch, files/2.0/start_orion.sh size mismatch, files/2.0.1/stop_orion.sh size mismatch, files/2.0.1/orion.conf size mismatch, files/2.0.1/start_orion.sh size mismatch

--- net-www/orion/files/2.0.1/start_orion.sh
+++ net-www/orion/files/2.0.1/start_orion.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# $Header: /usr/local/ssd/gentoo-x86/output/net-www/cvs-repo/gentoo-x86/net-www/orion/files/2.0.1/Attic/start_orion.sh,v 1.1 2003/05/01 17:50:29 absinthe Exp $
+# $Header: /usr/local/ssd/gentoo-x86/output/net-www/cvs-repo/gentoo-x86/net-www/orion/files/2.0.1/Attic/start_orion.sh,v 1.2 2004/07/18 04:26:56 dragonheart Exp $
 
 source /etc/conf.d/orion
 JAVAC=${JAVA_HOME}/bin/javac
--- net-www/orion/files/2.0.1/stop_orion.sh
+++ net-www/orion/files/2.0.1/stop_orion.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# $Header: /usr/local/ssd/gentoo-x86/output/net-www/cvs-repo/gentoo-x86/net-www/orion/files/2.0.1/Attic/stop_orion.sh,v 1.1 2003/05/01 17:50:29 absinthe Exp $
+# $Header: /usr/local/ssd/gentoo-x86/output/net-www/cvs-repo/gentoo-x86/net-www/orion/files/2.0.1/Attic/stop_orion.sh,v 1.2 2004/07/18 04:26:56 dragonheart Exp $
 ps auxww | grep orion.jar | awk '{print $2}' | xargs kill &> /dev/null
