c0f2486d71377322f89f5062cdad5f5b612bf431
Jon Hood <squinky86@gentoo.org>
2004-07-22 05:58:07 +0000

    Initial import.

---
+++ net-p2p/phex/files/phex.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Copyright 1999-2004 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+# $Header: /usr/local/ssd/gentoo-x86/output/net-p2p/cvs-repo/gentoo-x86/net-p2p/phex/files/Attic/phex.sh,v 1.1 2004/07/22 05:58:07 squinky86 Exp $
+
+cd /opt/phex
+java -jar phex.jar
