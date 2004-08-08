25059f785bcf54fce572f91d7df6367894a7e7c9
Stuart Herbert <stuart@gentoo.org>
2004-08-08 17:52:39 +0000

    Moving to www-servers/orion

--- net-www/orion/files/2.0.1/stop_orion.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# $Header: /usr/local/ssd/gentoo-x86/output/net-www/cvs-repo/gentoo-x86/net-www/orion/files/2.0.1/Attic/stop_orion.sh,v 1.2 2004/07/18 04:26:56 dragonheart Exp $
-ps auxww | grep orion.jar | awk '{print $2}' | xargs kill &> /dev/null
--- net-www/orion/files/2.0.2/stop_orion.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# $Header: /usr/local/ssd/gentoo-x86/output/net-www/cvs-repo/gentoo-x86/net-www/orion/files/2.0.2/Attic/stop_orion.sh,v 1.1 2004/07/16 14:00:35 axxo Exp $
-ps auxww | grep orion.jar | awk '{print $2}' | xargs kill &> /dev/null
