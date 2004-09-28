bab3bcb075b99c490a9ddc94f004d237747d4192
Sven Wegener <swegener@gentoo.org>
2004-09-28 02:02:09 +0000

    Gentoo Technologies, Inc. -> Gentoo Foundation

--- www-servers/monkeyd/files/monkeyd.init.d
+++ www-servers/monkeyd/files/monkeyd.init.d
@@ -1,7 +1,7 @@
 #!/sbin/runscript
-# Copyright 1999-2003 Gentoo Technologies, Inc.
+# Copyright 1999-2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /usr/local/ssd/gentoo-x86/output/www-servers/cvs-repo/gentoo-x86/www-servers/monkeyd/files/monkeyd.init.d,v 1.1 2004/08/08 17:41:38 stuart Exp $
+# $Header: /usr/local/ssd/gentoo-x86/output/www-servers/cvs-repo/gentoo-x86/www-servers/monkeyd/files/monkeyd.init.d,v 1.2 2004/09/28 02:02:09 swegener Exp $
 
 depend() {
 	need net
