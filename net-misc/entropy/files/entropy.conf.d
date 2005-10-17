9d8e5491a9d3194fdd26c034785b745b94579342
Mike Frysinger <vapier@gentoo.org>
2005-10-17 02:03:02 +0000

    moved to net-p2p/entropy_rsa

--- net-misc/entropy/files/entropy.conf.d
+++
@@ -1,6 +0,0 @@
-# Copyright 1999-2004 Gentoo Foundation
-# Distributed under the terms of the GNU General Public License v2
-# $Header: /usr/local/ssd/gentoo-x86/output/net-misc/cvs-repo/gentoo-x86/net-misc/entropy/files/Attic/entropy.conf.d,v 1.2 2004/07/14 23:57:55 agriffis Exp $
-
-# The user to run entropy as
-ENTROPY_USER="entropy"
