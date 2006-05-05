549d436fe50e89ba4da366d103c63f9310573049
Joerg Bornkessel <hd_brummy@gentoo.org>
2006-05-05 22:38:03 +0000

    version bump; old version removed Package-Manager: portage-2203-svn

--- media-plugins/vdr-pin/files/rc-addon.sh
+++
@@ -1,8 +0,0 @@
-# $Header: /usr/local/ssd/gentoo-x86/output/media-plugins/cvs-repo/gentoo-x86/media-plugins/vdr-pin/files/Attic/rc-addon.sh,v 1.1 2006/04/12 18:56:45 hd_brummy Exp $
-#
-# rc-addon-script for plugin pin
-#
-
-# DIR should dont exist on plugin start
-
-[[ -e /etc/vdr/plugins/pin ]] && rm -r /etc/vdr/plugins/pin
