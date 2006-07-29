a62af6294507fd7b8c640c174013d3704525f0f2
Diego Elio Pettenò <flameeyes@gentoo.org>
2006-07-29 15:34:18 +0000

    Create /var/run/pulse directory during install, thanks to Florian Steinel for reporting. Package-Manager: portage-2.1.1_pre4

--- media-sound/pulseaudio/files/pulseaudio.conf.d
+++ media-sound/pulseaudio/files/pulseaudio.conf.d
@@ -1,4 +1,5 @@
 # Config file for /etc/init.d/pulseaudio
+# $Header: /usr/local/ssd/gentoo-x86/output/media-sound/cvs-repo/gentoo-x86/media-sound/pulseaudio/files/pulseaudio.conf.d,v 1.6 2006/07/29 15:34:18 flameeyes Exp $
 
 # For more see "pulseaudio -h".
 
