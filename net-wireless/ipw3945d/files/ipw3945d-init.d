3c86b5d1a1124b5a041d742a319e9db0c431da74
Christian Heim <phreak@gentoo.org>
2006-09-09 07:53:40 +0000

    Hopefully finally fixing #142560 (thanks to Ryan Hill for the pointer on the forums thread). Removing obsolete versions. Package-Manager: portage-2.1.1_rc1-r7

--- net-wireless/ipw3945d/files/ipw3945d-init.d
+++ net-wireless/ipw3945d/files/ipw3945d-init.d
@@ -1,7 +1,7 @@
 #!/sbin/runscript
 # Copyright 1999-2006 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /usr/local/ssd/gentoo-x86/output/net-wireless/cvs-repo/gentoo-x86/net-wireless/ipw3945d/files/ipw3945d-init.d,v 1.1 2006/09/05 19:37:59 phreak Exp $
+# $Header: /usr/local/ssd/gentoo-x86/output/net-wireless/cvs-repo/gentoo-x86/net-wireless/ipw3945d/files/ipw3945d-init.d,v 1.2 2006/09/09 07:53:40 phreak Exp $
 
 PIDFILE=/var/run/ipw3945d/ipw3945d.pid
 
@@ -11,8 +11,7 @@ depend() {
 
 start() {
 	ebegin "Starting ipw3945d"
-	start-stop-daemon --start --exec /sbin/ipw3945d --chuid ipw3945d:ipw3945d \
-		--pidfile ${PIDFILE} -- \
+	start-stop-daemon --start --exec /sbin/ipw3945d --pidfile ${PIDFILE} -- \
 		--pid-file=${PIDFILE} ${ARGS}
 	eend ${?}
 }
