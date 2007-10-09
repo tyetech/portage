ae96befe1f479976455d196c0bb37a03c8fd3351
Joerg Bornkessel <hd_brummy@gentoo.org>
2007-10-09 14:57:54 +0000

    added rc-addon.sh to use xml config file in own config dir Package-Manager: portage-2.1.3.9

---
+++ media-plugins/vdr-menuorg/files/rc-addon.sh
@@ -0,0 +1,12 @@
+# $Header: /usr/local/ssd/gentoo-x86/output/media-plugins/cvs-repo/gentoo-x86/media-plugins/vdr-menuorg/files/rc-addon.sh,v 1.1 2007/10/09 14:57:54 hd_brummy Exp $
+#
+# rc-addon plugin-startup-skript for vdr-menuorg
+#
+# Joerg Bornkessel <hd_brummy@gentoo.org>
+
+plugin_pre_vdr_start() {
+
+    add_plugin_param "-c /etc/vdr/plugins/menuorg/menuorg.xml"
+}
+
+
