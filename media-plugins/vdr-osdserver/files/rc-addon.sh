5cddc7c8c42713988c19556cd4b0cff46f7ef4cc
Joerg Bornkessel <hd_brummy@gentoo.org>
2007-12-02 16:02:37 +0000

    initial ebuild, tanks to Guido Zuliani on bug 200903 Package-Manager: portage-2.1.3.19

---
+++ media-plugins/vdr-osdserver/files/rc-addon.sh
@@ -0,0 +1,11 @@
+# $Header: /usr/local/ssd/gentoo-x86/output/media-plugins/cvs-repo/gentoo-x86/media-plugins/vdr-osdserver/files/rc-addon.sh,v 1.1 2007/12/02 16:02:37 hd_brummy Exp $
+#
+# rc-addon plugin-startup-skript for vdr-osdserver
+#
+# zulio <zulio(at)zulinux.net>
+
+plugin_pre_vdr_start() {
+
+	add_plugin_param "-p ${OSDSERVER_PORT:=2010}"
+}
+
